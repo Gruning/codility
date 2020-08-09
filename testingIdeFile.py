a3e21b8c8d145a407b4dbc06ca1677384a4e61e3
Alberto Gruning <alberto.gruning@ndvsistemas.com>
2020-08-09 16:46:40 -0300

    first hint for missing element starting challenge

--- testingIdeFile.py
+++ testingIdeFile.py
@@ -1,8 +1,11 @@
 print('perm missing element')
 
-array: [2, 3, 1, 5]
+givenArray: [2, 3, 1, 5]
 
 # for x in 1 to n + 1
 #   x in input
 #   if no found
 #   return x
+
+for x in range(1, len(givenArray + 1)):
+    print(x)
