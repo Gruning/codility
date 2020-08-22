91f70bf07bacf08223ed3a10cc5bd70040616e6f
Alberto Gruning <alberto.gruning@ndvsistemas.com>
2020-08-22 00:38:05 -0300

    first hint code python - fix error

--- permMissingElem_FirstHint.py
+++ permMissingElem_FirstHint.py
@@ -1,11 +1,12 @@
 input = [2, 3, 1, 5]
-n = len(input) + 1
+n = len(input)
 result = 0
 
-for x in n:
+for x in range(1, n + 1):
     found = False
     for i in input:
-        if i == x:
+        print(f'{x} -- {i}')
+        if x == i:
             found = True
             result = i
 print(result)
