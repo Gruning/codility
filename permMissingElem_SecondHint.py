8722b5a44a190b0463772a05af6673b4f409daa0
Alberto Gruning <alberto.gruning@ndvsistemas.com>
2020-08-30 13:53:57 -0300

    sec2Cap5_permMissingElemen_debugging

--- permMissingElem_SecondHint.py
+++ permMissingElem_SecondHint.py
@@ -3,10 +3,13 @@ secondArray = [None] * (len(input) + 2)
 print(len(secondArray))
 
 for x in input:
-    secondArray[x] = False
+    secondArray[input[x]] = False
 
-notlast = len(secondArray) - 0
+print(secondArray)
+
+notlast = len(secondArray) - 1
 
 for y in range(1, notlast):
+    print(secondArray[y])
     if secondArray[y]:
         print(y)
