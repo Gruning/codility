72ce0f5cdc3ab049abd7be10f2cba057c4672fc3
Alberto Gruning <alberto.gruning@ndvsistemas.com>
2020-08-31 00:10:59 -0300

    sec2cap6_permMissing_secondHintOK

--- permMissingElem_SecondHint.py
+++ permMissingElem_SecondHint.py
@@ -1,15 +1,16 @@
 input = [2, 3, 1, 5]
 secondArray = [None] * (len(input) + 2)
+lenSecond = len(secondArray)
 print(len(secondArray))
 
 for x in input:
-    secondArray[input[x]] = False
+    secondArray[x] = False
 
 print(secondArray)
 
 notlast = len(secondArray) - 1
 
-for y in range(1, notlast):
-    print(secondArray[y])
-    if secondArray[y]:
-        print(y)
+for y in range(1, lenSecond):
+    #print(secondArray[y])
+    if secondArray[y] is None:
+        print(f'missing ', y)
