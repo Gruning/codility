0d139dde24b7295ff41938a00fed9eca48c754b1
Alberto Gruning <alberto.gruning.zen@gmail.com>
2020-09-20 09:25:21 -0300

    equilibriumFirstH

--- equilibrium/hint1.py
+++ equilibrium/hint1.py
@@ -1,12 +1,14 @@
 p =[3,1,2,4,3]
 right =[]
 left = p
+sumLeft = 0
 
 for value in p:
     right.append(value)
     del left[p[value]]
     for l in left:
-        abs(sum = sum + left )
+        sumLeft = sumLeft + left
+        print('sum Left ' + sumLeft) 
 
-    for r in righ:
+    for r in right:
         abs(sum = sum + right )
