0d5f12e95cc51e39dd57fff2b3e7d816617a5de7
AlbertoGruning <alberto.gruning.zen@gmail.com>
2020-09-23 19:14:28 -0300

    equilibrium

--- equilibrium/hint1.py
+++ equilibrium/hint1.py
@@ -2,13 +2,17 @@ p =[3,1,2,4,3]
 right =[]
 left = p
 sumLeft = 0
+sumRight = 0
 
 for value in p:
     right.append(value)
     del left[p[value]]
+
     for l in left:
         sumLeft = sumLeft + l
-        print('sum Left {0}',sumLeft) 
+        print('sum Left ',sumLeft) 
 
     for r in right:
-        abs(sum = sum + right )
+
+        print('sum right ',sumRight) 
+        #abs(sum = sum + right )
