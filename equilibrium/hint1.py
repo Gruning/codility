696fd4d44bf5339f4f4820548fbc9063c91b4f4a
AlbertoGruning <alberto.gruning.zen@gmail.com>
2020-09-24 20:39:46 -0300

    fequilibrium-hint1-arrays

--- equilibrium/hint1.py
+++ equilibrium/hint1.py
@@ -1,18 +1,22 @@
 p =[3,1,2,4,3]
-right =[]
-left = p
+right = p
+left = []
 sumLeft = 0
 sumRight = 0
+cont = 0
+
 
 for value in p:
-    right.append(value)
-    del left[p[value]]
+    left.append(value)
+    print('left',left)
+    del right[cont]
+    cont = cont + 1
+    print('right',right)
+    # for l in left:
+    #     sumLeft = sumLeft + l
+    #     print('sum Left ',sumLeft) 
 
-    for l in left:
-        sumLeft = sumLeft + l
-        print('sum Left ',sumLeft) 
-
-    for r in right:
-
-        print('sum right ',sumRight) 
+    # for r in right:
+    #     if right != []:
+    #         print('sum right ',sumRight) 
         #abs(sum = sum + right )
