73d8a8cc0ec356bf6879985945c0e4420967630e
AlbertoGruning <alberto.gruning.zen@gmail.com>
2020-09-27 13:58:41 -0300

    iterator was altered

--- equilibrium/hint1.py
+++ equilibrium/hint1.py
@@ -3,18 +3,20 @@ right = p
 left = []
 sumLeft = 0
 sumRight = 0
-cont = 0
+# _cont = 0
 
 print('p',p)
 for value in p:
     print('value', value)
-    left.append(value)
-    print('cont', cont)
-    print('array', value)
-    print('left',left)
-    del right[cont]
-    cont = cont + 1
-    print('right',right)
+    # left.append(value)
+    # print('cont', _cont)
+    # print('array', value)
+    # print('left',left)
+    # del right[_cont]
+    # _cont = _cont + 1
+    # print('right',right)
+
+
     # print('right',right)
 
     # for l in left:
