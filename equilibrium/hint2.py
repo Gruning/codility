21396f52baf0c0868b68f1a3de7ea70ecfb97e34
AlbertoGruning <alberto.gruning.zen@gmail.com>
2020-10-11 10:53:49 -0300

    second hint

--- equilibrium/hint2.py
+++ equilibrium/hint2.py
@@ -1,11 +1,14 @@
 p =[3,1,2,4,3]
-right=[]
-for r in p:
-    right.append(r)
-
-left = []
-sumLeft = 0
-sumRight = 0
-
 _sum = sum(p)
+right=[]
+left = []
+sumLeft = _sum
+sumRight = _sum
+for r in range (1,len(p)):
+    print('r ',r )
+    print('p[r] ', p[r])
+    sumRight = sumRight - p[r-1]
+    print('sumrigth ', sumRight)
+
+
 print(_sum)
