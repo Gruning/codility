8d468a6c9d1ff8d6ea5a3efbce6d55566d66d3b1
AlbertoGruning <alberto.gruning.zen@gmail.com>
2020-10-18 19:40:28 -0300

    13-equilibrium-solution

--- equilibrium/solution.py
+++ equilibrium/solution.py
@@ -4,4 +4,14 @@ def solution(A):
     dif = abs(sum_left - sum_right)
 
     for i in range(1,len(A)-1):
-        pass
+        sum_left +=A[i]
+        sum_right -=A[i]
+        current_diff = abs(sum_left - sum_right)
+        if diff > current_diff:
+            dif = current_dif
+        
+    return diff
+
+#test
+
+print(solution([3,1,2,4,3]))
