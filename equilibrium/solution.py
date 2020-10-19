ab7669e645b9483bf33040142b8fc0b61395e0ee
AlbertoGruning <alberto.gruning.zen@gmail.com>
2020-10-19 19:10:20 -0300

    fix

--- equilibrium/solution.py
+++ equilibrium/solution.py
@@ -1,14 +1,14 @@
 def solution(A):
     sum_left= A[0]
     sum_right= sum(A) - A[0]
-    dif = abs(sum_left - sum_right)
+    diff = abs(sum_left - sum_right)
 
     for i in range(1,len(A)-1):
         sum_left +=A[i]
         sum_right -=A[i]
         current_diff = abs(sum_left - sum_right)
         if diff > current_diff:
-            dif = current_dif
+            dif = current_diff
         
     return diff
 
