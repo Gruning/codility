6b633743c097804f26e16311aa7ae259473d471e
kimono-server <alberto.gruning.zen@gmail.com>
2020-11-02 22:15:25 +0000

    cap15-ciclic

--- ciclicRotation/hint1.py
+++ ciclicRotation/hint1.py
@@ -2,5 +2,8 @@ a=[1,2,3,4,5]
 k=2
 def solution(a,k):
     origLenth = len(a)
-    newArrayLen = origLenth + k
+    if(k<2*len(a))
+        newArrayLen = origLenth + k
     newArray = [0]* newArrayLen
+    
+    return newArrayLen
