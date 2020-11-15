92fc35a6204f0af4c1d43354e437175557e9ca5b
kimono-server <alberto.gruning.zen@gmail.com>
2020-11-15 17:38:08 +0000

    ciclic-solved-TODO-Funtion

--- ciclicRotation/hint1.py
+++ ciclicRotation/hint1.py
@@ -1,11 +1,17 @@
 a=[1,2,3,4,5]
-k=2
-def solution(a,k):
-    origLenth = len(a)
-    leftOver = k % origLenth
+k=7
+#def solution(a,k):
+#origLenth = len(a)
+print(len(a))
+leftover =  k % len(a)
+print(leftover)
+print('-------')
+#newArray = a 
 
-    if leftOver != 0:
-        newArrayLen = origLenth + leftOver
-    newArray = [0]* newArrayLen
-    
-    return newArrayLen
+if leftover != 0:
+    #newArrayLen = origLenth + leftOver
+    for elem in range(leftover , len(a)):
+        print(a[elem])        
+    for elem in range(leftover):
+        print(a[elem])
+#return newArray
