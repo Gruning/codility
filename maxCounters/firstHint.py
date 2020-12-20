755d461d4d8075d18156630ed2abdf23e9cf3cb6
kimono-server <alberto.gruning.zen@gmail.com>
2020-12-20 19:36:39 +0000

    cap20-maxCounters-iterations

--- maxCounters/firstHint.py
+++ maxCounters/firstHint.py
@@ -3,7 +3,16 @@ a=[3,4,4,6,1,4,4]
 
 def solution(n,a):
     print(a)
+    array = [0]* n
+   # for cont in array:
+    #    cont = 0
 
-    array = [None]* n
+    print(array)
+
+    for item in a:        
+        print (item)
+       #array[item] = array[item] + 1
+        
+
 solution(n,a)
 
